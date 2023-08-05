87130834cbee48282eb011bc70cf3871bdb6f839
marait123 <mohammedibrahimgaballah@gmail.com>
2023-08-05 14:24:11 +0300

    added docker-compose and gitignore

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
 from flask import Flask, jsonify
 from itsdangerous import json
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return jsonify(greeting="hello 1", code=123)
+    return jsonify(greeting="welcome 1 121 1", code=123)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0",  debug=True)
 
 # E:\udacity\sessions\session 4\codes\app.py
 # build the aimge
 #  docker build -t  flaskimage:3 .
 # running docker container
-#  docker run --name flask_app -p 5001:5000 --mount type=bind,source="$(pwd)",target=/app -d flask:latest
+#  docker run --name flask_app -p 5000:5000 --mount type=bind,source="$(pwd)",target=/app -d flaskimage:3
+#  docker run --name flask_app -p 5000:5000 -d flaskimage:3
